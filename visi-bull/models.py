3f2b8696a00e4034d70bd577433ef38711c6996b
unknown <SimonCB765@gmail.com>
2014-09-12 13:53:40 +0100

    Updated the page models

--- visi-bull/models.py
+++ visi-bull/models.py
@@ -4,7 +4,7 @@ class PrettyPicture(ndb.Model):
     """Contains the information about a visualisation page."""
 
     postName = ndb.StringProperty(required=True)  # The name of the page.
-    postDate = ndb.DateProperty(auto_now_add=True)  # The date when the page was posted/created.
+    postDate = ndb.DateTimeProperty(auto_now_add=True)  # The date when the page was posted/created.
     primaryTag = ndb.StringProperty(required=True)  # The primary tag of the post (used to identify the URL for the post).
     isDemo = ndb.BooleanProperty(required=True)  # Whether the page is a demo.
     isInfo = ndb.BooleanProperty(required=True)  # Whether the page is an infographic.
