a192638d0cb3d2a8b2028ccc8220942baeb0403a
unknown <SimonCB765@gmail.com>
2014-09-25 16:01:38 +0100

    Added hideous placeholder colors

--- visi-bull/pages/styled_lists/styled_lists.js
+++ visi-bull/pages/styled_lists/styled_lists.js
@@ -1,15 +1,22 @@
 $(document).ready(function()
 {
     // Hide all but the first tab of content.
-    var currentlyVisibile = $(".navigation li:first").attr("data-navLink");
-    console.log(currentlyVisibile);
+	var selectedNavTab = $(".navigation li:first");
+	selectedNavTab.toggleClass("selected");
+    var selectedNavTabID = selectedNavTab.attr("data-navLink");
     $(".navigation-panel:not(:first)").hide();
 
     // Setup navigation tab behaviour.
     $(".navigation li").click(function()
         {
-            $(".navigation-panel#" + currentlyVisibile).hide();
-            currentlyVisibile = $(this).attr("data-navLink");
-            $(".navigation-panel#" + currentlyVisibile).show();
+			// Clear old selected navigation tab information.
+            $(".navigation-panel#" + selectedNavTabID).hide();
+			selectedNavTab.toggleClass("selected");
+			
+			// Record new selected navigation tab information.
+			selectedNavTab = $(this);
+			selectedNavTab.toggleClass("selected");
+            selectedNavTabID = selectedNavTab.attr("data-navLink");
+            $(".navigation-panel#" + selectedNavTabID).show();
         });
 });
