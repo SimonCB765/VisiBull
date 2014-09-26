69cc0fb01150d03f7347c0d94a5113ea21368e64
unknown <SimonCB765@gmail.com>
2014-09-26 17:21:53 +0100

    Border and shading style buttons created

--- visi-bull/pages/styled_lists/styled_lists.js
+++ visi-bull/pages/styled_lists/styled_lists.js
@@ -1,10 +1,11 @@
 $(document).ready(function()
 {
-    // Hide all but the first tab of content.
-	var selectedNavTab = $(".navigation li:first");
+    // Hide all but the desired initial tab of content.
+	var startingTabIndex = 1;
+	var selectedNavTab = $(".navigation li:eq(" + startingTabIndex + ")");
 	selectedNavTab.toggleClass("selected");
     var selectedNavTabID = selectedNavTab.attr("data-navLink");
-    $(".navigation-panel:not(:first)").hide();
+    $(".navigation-panel:not(:eq(" + startingTabIndex + "))").hide();
 
     // Setup navigation tab behaviour.
     $(".navigation li").click(function()
@@ -18,5 +19,21 @@ $(document).ready(function()
 			selectedNavTab.toggleClass("selected");
             selectedNavTabID = selectedNavTab.attr("data-navLink");
             $(".navigation-panel#" + selectedNavTabID).show();
+        });
+
+	// Setup the behaviour for the first set of buttons.
+	var selectedButton = $("#button-set-1 li:first");
+	selectedButton.toggleClass("selected")
+    $("#button-set-1 li").click(function()
+        {
+			// Undepress button.
+			selectedButton.toggleClass("selected");
+			
+			// Depress clicked button.
+			selectedButton = $(this);
+			selectedButton.toggleClass("selected");
+			
+			// change description color.
+			$("#button-description-1").css("color", selectedButton.attr("data-color"));
         });
 });
