47a37097880a33ea2eee1cb910ddb57bd57432dc
unknown <SimonCB765@gmail.com>
2014-10-02 08:23:59 +0100

    Navigation buttons and concept styles added

--- visi-bull/pages/page_navigation/page_navigation.js
+++ visi-bull/pages/page_navigation/page_navigation.js
@@ -2,13 +2,13 @@ $(document).ready(function()
 {
     // Hide all but the desired initial tab of content.
 	var startingTabIndex = 1;
-	var selectedNavTab = $(".navigation li:eq(" + startingTabIndex + ")");
+	var selectedNavTab = $(".navigation .button:eq(" + startingTabIndex + ")");
 	selectedNavTab.toggleClass("selected");
     var selectedNavTabID = selectedNavTab.attr("data-navLink");
     $(".navigation-panel:not(:eq(" + startingTabIndex + "))").hide();
 
     // Setup navigation tab behaviour.
-    $(".navigation li").click(function()
+    $(".navigation .button").click(function()
         {
 			// Clear old selected navigation tab information.
             $(".navigation-panel#" + selectedNavTabID).hide();
@@ -21,35 +21,51 @@ $(document).ready(function()
             $(".navigation-panel#" + selectedNavTabID).show();
         });
 
-	// Setup the behaviour for the first set of buttons.
-    $("#button-set-1 li")
-		.on( "mousedown", function()
-			{
-				// Change button text color.
-				var selectedButton = $(this);
-				$("#button-set-1 li").css("color", selectedButton.attr("data-color"));
-			});
+	// Setup behaviour for button set 1.
+	var selectedButtonSet1 = $("#button-set-1 .button:eq(0)");
+	$("#button-set-1 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet1.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet1 = $(this);
+			selectedButtonSet1.addClass("selected");
+		});
 
-	// Setup the behaviour for the second set of buttons.
-	var selectedButtonSet2 = $("#button-set-2 li:first");
-	selectedButtonSet2.toggleClass("selected")
-    $("#button-set-2 li")
-		.on("mousedown", function()
-			{
-				// Undepress button.
-				selectedButtonSet2.toggleClass("selected");
-				
-				// Depress clicked button.
-				selectedButtonSet2 = $(this);
-				selectedButtonSet2.toggleClass("selected");
-			});
+	// Setup behaviour for button set 2.
+	var selectedButtonSet2 = $("#button-set-2 .button:eq(0)");
+	$("#button-set-2 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet2.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet2 = $(this);
+			selectedButtonSet2.addClass("selected");
+		});
 
-	// Setup the behaviour for the third set of buttons.
-    $("#button-set-3 li").on("mousedown", function() { $(this).toggleClass("selected"); });
-	
-	// Setup the behaviour for the fourth set of buttons.
-    $("#button-set-4 li").on("mousedown", function() { $(this).toggleClass("selected"); });
-	
-	// Setup the behaviour for the fifth set of buttons.
-    $("#button-set-5 li").on("mousedown", function() { $(this).toggleClass("selected"); });
+	// Setup behaviour for button set 3.
+	var selectedButtonSet3 = $("#button-set-3 .button:eq(0)");
+	$("#button-set-3 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet3.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet3 = $(this);
+			selectedButtonSet3.addClass("selected");
+		});
+
+	// Setup behaviour for button set 4.
+	var selectedButtonSet4 = $("#button-set-4 .button:eq(0)");
+	$("#button-set-4 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet4.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet4 = $(this);
+			selectedButtonSet4.addClass("selected");
+		});
 });
