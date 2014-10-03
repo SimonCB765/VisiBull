c10e3f10f7abd8b970161af167c8216457ab0827
unknown <SimonCB765@gmail.com>
2014-10-03 17:28:49 +0100

    Added examples of all button types

--- visi-bull/pages/page_navigation/page_navigation.js
+++ visi-bull/pages/page_navigation/page_navigation.js
@@ -2,13 +2,13 @@ $(document).ready(function()
 {
     // Hide all but the desired initial tab of content.
 	var startingTabIndex = 1;
-	var selectedNavTab = $(".navigation .button:eq(" + startingTabIndex + ")");
+	var selectedNavTab = $("#navigation-buttons .button:eq(" + startingTabIndex + ")");
 	selectedNavTab.toggleClass("selected");
     var selectedNavTabID = selectedNavTab.attr("data-navLink");
     $(".navigation-panel:not(:eq(" + startingTabIndex + "))").hide();
 
     // Setup navigation tab behaviour.
-    $(".navigation .button").click(function()
+    $("#navigation-buttons .button").click(function()
         {
 			// Clear old selected navigation tab information.
             $(".navigation-panel#" + selectedNavTabID).hide();
@@ -67,5 +67,65 @@ $(document).ready(function()
 			// Record new selected button information.
 			selectedButtonSet4 = $(this);
 			selectedButtonSet4.addClass("selected");
+		});
+	
+	// Setup behaviour for button set 5.
+	var selectedButtonSet5 = $("#button-set-5 .button:eq(0)");
+	$("#button-set-5 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet5.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet5 = $(this);
+			selectedButtonSet5.addClass("selected");
+		});
+	
+	// Setup behaviour for button set 6.
+	var selectedButtonSet6 = $("#button-set-6 .button:eq(0)");
+	$("#button-set-6 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet6.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet6 = $(this);
+			selectedButtonSet6.addClass("selected");
+		});
+	
+	// Setup behaviour for button set 7.
+	var selectedButtonSet7 = $("#button-set-7 .button:eq(0)");
+	$("#button-set-7 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet7.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet7 = $(this);
+			selectedButtonSet7.addClass("selected");
+		});
+	
+	// Setup behaviour for button set 8.
+	var selectedButtonSet8 = $("#button-set-8 .button:eq(0)");
+	$("#button-set-8 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet8.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet8 = $(this);
+			selectedButtonSet8.addClass("selected");
+		});
+	
+	// Setup behaviour for button set 9.
+	var selectedButtonSet9 = $("#button-set-9 .button:eq(0)");
+	$("#button-set-9 .button").click(function()
+		{
+			// Clear old selected button information.
+            selectedButtonSet9.removeClass("selected");
+			
+			// Record new selected button information.
+			selectedButtonSet9 = $(this);
+			selectedButtonSet9.addClass("selected");
 		});
 });
