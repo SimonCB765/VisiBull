a876cd03b401eb1caf91fb8d0ded22c43c047909
unknown <SimonCB765@gmail.com>
2014-10-30 17:57:04 +0000

    Outer ring of zone 2

--- visi-bull/pages/london_underground/london_underground.js
+++ visi-bull/pages/london_underground/london_underground.js
@@ -5,8 +5,10 @@ $(document).ready(function()
     // Draw the zones.
     underground.append("path")
         .classed("zone", true)
-        .attr("d", create_zone_8)
-        ;
+        .attr("d", create_zone_8());
+    underground.append("path")
+        .classed("zone", true)
+        .attr("d", create_zone_2());
 
 
 
@@ -14,36 +16,128 @@ $(document).ready(function()
         .on("click", function() { console.log(d3.mouse(d3.select("body").node())); });
 
 
+	function create_zone_2()
+	{
+		var zone2Outer =
+			"M490,130" +  // Just above Hampstead Heath.
+			"L668,130" +
+			"Q672,130" + ",676,132" +  // Bend by Seven Sisters.
+			"L787,233" +
+			"Q795,239" + ",795,244" +  // Bend by Pudding Mill Lane.
+			"L795,363" +
+			"Q795,366" + ",791,366" +  // First bend near North Greenwich.
+			"Q788,366" + ",788,371" +  // Second bend near North Greenwich.
+			"L788,425" +
+			"Q788,429" + ",785,431" +  // Bend just below the 2.
+			"L761,452" +
+			"Q757,455" + ",757,459" +  // Bend by Greenwich.
+			"L757,486" +
+			"Q756,494" + ",745,498" +  // Bend by Lewisham.
+			"L720,498" +
+			"Q715,497" + ",711,494" +  // Bend right of Forest Hill.
+			"L695,480" +
+			"Q692,478" + ",688,478" +  // Bend right of Brockley.
+			"L634,478" +
+			"Q630,479" + ",627,482" +  // Bend left of Brockley.
+			"L595,512" +
+			"Q592,515" + ",586,515" +  // Bend below and to the right of Denmark Hill.
+			"L417,515" +
+			"Q412,515" + ",410,513" +  // Bend left of Clapham South.
+			"L337,446" +
+			"Q334,443" + ",327,443" +  // Bend at East Putney.
+			"L304,443" +
+			"Q300,442" + ",298,440" +  // Bend left of East Putney.
+			"L225,373" +
+			"Q223,370" + ",223,365" +  // Bend at Turnham Green.
+			"L223,360" +
+			"Q223,356" + ",226,353" +  // Bend just above Turnham Green.
+			"L242,339" +
+			"Q245,336" + ",245,330" +  // Bend right of South Action.
+			"L245,221" +
+			"Q245,215" + ",248,213" +  // Bend left of Kilburn Park.
+			"L275,188" +
+			"Q277,186" + ",280,186" +  // Bend left of Willesden Junction.
+			"L289,186" +
+			"Q294,186" + ",296,183" +  // Bend right of Willesden Junction.
+			"L312,169" +
+			"Q315,166" + ",320,166" +  // Bend above Kensal Rise.
+			"L400,166" +
+			"Q405,166" + ",408,163" +  // Bend right of Willesden Green.
+			"L425,147" +
+			"Q428,145" + ",432,145" +  // Bend left of Hampstead.
+			"L465,145" +
+			"Q469,145" + ",473,141" +  // Bend at Hampstead.
+			"L483,132" +
+			"Q486,130" + ",490,130";  // Bend above Hampstead Heath.
+		var zone2Inner =
+			"M333,245" +  // Below Paddington.
+			"C316,245" + ",316,219" + ",336,218" +  // Bend around the left of Paddington.
+			"L517,218" +
+			"Q520,218" + ",523,216" +  // Bend below Morning Crescent.
+			"L535,205" +
+			"Q537,203" + ",541,203" +  // Bend above King's Cross.
+			"L583,203" +
+			"Q588,203" + ",590,205" +  // Bend below and left of Caledonian Road.
+			"L627,239" +
+			"Q629,241" + ",635,242" +  // Bend above and right of Old Street.
+			"L694,242" +
+			"Q703,244" + ",705,251" +  // Bend at Hoxton.
+			"L705,275" +
+			"Q705,278" + ",702,281" +  // Bend above and left of Stepney Green.
+			"L696,287" +
+			"Q693,289" + ",693,293" +  // Bend above and left of Whitechapel.
+			"L693,341" +
+			"Q693,349" + ",683,349" +  // Bend left of Wapping.
+			"Q676,350" + ",676,352" +
+			"Q676,355" + ",669,355" +
+			"L656,355" +
+			"Q653,355" + ",649,359" +  // Bend above Bermondsey.
+			"L555,444"
+			//+
+			//"L660,343"
+			//+
+			//"Q693,351" + ",675,354" 
+			//+
+			//"L660,354"
+			
+			/*
+			"L342,271" +
+			"L342,257" +
+			"Q342,246" + ",333,245"  // Bend below Paddington.
+			*/
+			;
+		return zone2Outer + zone2Inner;
+	}
 
     function create_zone_8()
     {
-        var zone9 =
+        var zone8 =
             "M2,113" +
             "L85,36" +
-            "Q88,34" + ",88,30" +
+            "Q88,34" + ",88,30" +  // Bend just below the 9.
             "L88,7" +
             "L214,7" +
             "L240,31" +
-            "Q243,33" + ",246,33" +
+            "Q243,33" + ",246,33" +  // Bend to the left of Watford Junction.
             "L308,33" +
-            "Q311,33" + ",314,31" +
+            "Q311,33" + ",314,31" +  // Bend to the right of Watford Junction.
             "L339,7" +
             "L370,7" +
             "L321,52" +
-            "Q317,55" + ",313,55" +
+            "Q317,55" + ",313,55" +  // Bend to the right of Bushey.
             "L257,55" +
-            "Q251,55" + ",249,52" +
+            "Q251,55" + ",249,52" +  // Bend to the left of Bushey.
             "L211,17" +
-            "Q207,15" + ",204,15" +
+            "Q207,15" + ",204,15" +  // Bend to the right of the 7.
             "L179,15" +
-            "Q172,15" + ",170,17" +
+            "Q172,15" + ",170,17" +  // Bend to the left of the 7.
             "L139,45" +
-            "Q133,49" + ",129,49" +
+            "Q133,49" + ",129,49" +  // Bend at Chorleywood.
             "L88,49" +
-            "Q83,49" + ",80,52" +
+            "Q83,49" + ",80,52" +  // Bend a little farther below the 9 than the first one.
             "L2,123" +
             "L2,113"
             ;
-        return zone9;
+        return zone8;
     }
 });
