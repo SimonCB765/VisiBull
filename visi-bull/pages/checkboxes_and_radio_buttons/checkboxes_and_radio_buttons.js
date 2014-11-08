daf7b7cfb0e9ab90ddd5faa23cdf8178db0d5bdb
unknown <SimonCB765@gmail.com>
2014-11-08 20:45:09 +0000

    Tick boxes complete

--- visi-bull/pages/checkboxes_and_radio_buttons/checkboxes_and_radio_buttons.js
+++ visi-bull/pages/checkboxes_and_radio_buttons/checkboxes_and_radio_buttons.js
@@ -3,7 +3,7 @@ $(document).ready(function()
     // Create the checkboxes and radio buttons.
     create_tick_boxes("#tickbox");
     create_cross_boxes("#crossbox");
-    create_radio_buttons("#radio");
+    create_scribble_boxes("#scribblebox");
     create_fancy_radio("#fancyradio");
 
     /*********************
@@ -33,7 +33,7 @@ $(document).ready(function()
             .attr("height", svgHeight);
     }
 
-    function create_radio_buttons(svgID)
+    function create_scribble_boxes(svgID)
     {
         // Definitions needed.
         var svgWidth = 600;  // The width of the SVG element.
@@ -55,8 +55,9 @@ $(document).ready(function()
         var boxLabelGap = 5;  // The gap between the box and the label.
         var choiceGap = 20;  // The gap between each checkbox/label combos.
         var startXPos = 20;  // The minimum position on the x axis at which the boxes can begin being placed.
-        var stopXPos = 500;  // The maximum position on the x axis at which the boxes can begin being placed.
-        var heightToFill = 90;  // The height that the checkboxes should evenly fill up (i.e. one row would be right in the middle, 3 rows at the quartiles).
+        var widthToFill = 560;  // The width of the space that the checkboxes should fill up.
+        var startYPos = 10;  // The Y position of the top of the first row of boxes.
+        var heightToFill = 80;  // The height that the checkboxes should evenly fill up (i.e. one row would be right in the middle, 3 rows at the quartiles).
         var checkMark;  // The mark that indicates that a box has been checked.
         var labels = ["Green", "Orange", "Pink", "Purple", "Seafoam Green", "Sky Blue", "Yellow"];  // Labels for the checkboxes.
         var colorCodes = ["#00FF00", "#FFAF1A", "#FF008C", "#AE2DE3", "#00FF7B", "#00FFFF", "#FFFF00"];  // Color codes corresponding to the label names.
@@ -83,12 +84,34 @@ $(document).ready(function()
         // Compute the positions of the checkboxes.
         // Set the boxes on the half pixel to make them crisper with a 1px border.
         var choiceData = [];
+        var choiceWidth;  // The width of the current box/label combo.
+        var numberOfRows = 1;  // The number of rows of checkboxes to use.
         var cumulativeWidth = startXPos;
         for (var i = 0; i < labels.length; i++)
         {
-            choiceData.push({"color": colorCodes[i], "label": labels[i], "transX": cumulativeWidth, "transY": 30, "y": 0.5});
-            cumulativeWidth += (boxSize + boxStrokeWidth + boxLabelGap + labelWidths[i] + choiceGap);
+            choiceWidth = boxSize + boxStrokeWidth + boxLabelGap + labelWidths[i] + choiceGap;
+            if (cumulativeWidth + choiceWidth > widthToFill)
+            {
+                numberOfRows++;
+                cumulativeWidth = startXPos;
+            }
+            choiceData.push({"color": colorCodes[i], "label": labels[i], "rowNumber": numberOfRows, "transX": cumulativeWidth});
+            cumulativeWidth += choiceWidth;
         }
+        var rowGap = heightToFill / (numberOfRows + 1);  // The gaps to leave between each row.
+        for (var i = 0; i < choiceData.length; i++)
+        {
+            var currentChoice = choiceData[i];
+            currentChoice.transY = rowGap * currentChoice.rowNumber;
+        }
+
+        // Add the lines showing the boundaries for the checkboxes.
+        svg.append("rect")
+            .classed("outline", true)
+            .attr("x", startXPos)
+            .attr("y", startYPos)
+            .attr("width", widthToFill)
+            .attr("height", heightToFill);
 
         // Add the checkboxes.
         var boxOffset = boxStrokeWidth / 2;  // Offset each checkbox by half the stroke width to keep everything within the desired bounds.
@@ -101,7 +124,7 @@ $(document).ready(function()
         choiceContainers.append("rect")  // The checkbox squares.
             .classed("checkbox", true)
             .attr("x", boxOffset)
-            .attr("y", function(d) { return d.y; })
+            .attr("y", boxStrokeWidth / 2)
             .attr("width", boxSize)
             .attr("height", boxSize)
             .style("stroke-width", boxStrokeWidth);
@@ -109,7 +132,7 @@ $(document).ready(function()
             .attr("x", boxSize + boxStrokeWidth + boxLabelGap)
             .attr("y", Math.ceil((boxSize + boxStrokeWidth) * 4 / 5))
             .text(function(d) { return d.label; });
-        choiceContainers.append("rect")  // An invisible rectangle to catch events.
+        choiceContainers.append("rect")  // An invisible rectangle to help catch events.
             .classed("backing", true)
             .attr("x", 0)
             .attr("y", 0)
@@ -125,20 +148,40 @@ $(document).ready(function()
                 // Change colors and add the mark to indicate selection.
                 if (container.classed("selected"))
                 {
+                    // The checkbox was selected, so now deselct it.
                     container.classed("selected", false);
                     containedBox.style("stroke", "white");
                     containedLabel.style("fill", "white");
-                    container.select(".checkmark").remove();
+                    var checkmark = container.select(".checkmark");
+                    var checkmarkLength = checkmark.node().getTotalLength();
+                    checkmark
+                        .style("stroke", "white")
+                        .transition()
+                        .duration(300)
+                        .ease("linear")
+                        .style("stroke-dashoffset", checkmarkLength)
+                        .remove();
                 }
                 else
                 {
+                    // The checkbox was not selected, so now select it.
                     container.classed("selected", true);
                     containedBox.style("stroke", d.color);
                     containedLabel.style("fill", d.color);
-                    container.append("path")
+                    var checkmark = container.append("path")
                         .classed("checkmark", true)
                         .attr("d", checkMark)
                         .style("stroke", d.color);
+
+                    // Transition the checkmark so it doesn't appear all at once.
+                    var checkmarkLength = checkmark.node().getTotalLength();
+                    checkmark
+                        .style("stroke-dasharray", checkmarkLength + " " + checkmarkLength)
+                        .style("stroke-dashoffset", checkmarkLength);
+                    checkmark.transition()
+                        .duration(300)
+                        .ease("linear")
+                        .style("stroke-dashoffset", 0);
                 }
             });
     }
