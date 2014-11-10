7ea4cd2c622fb083ba1b2b4334ecb3f8ffa8d73a
unknown <SimonCB765@gmail.com>
2014-11-10 09:24:50 +0000

    Crude addition of vertical checkboxes/radio buttons

--- visi-bull/pages/checkboxes_and_radio_buttons/checkboxes_and_radio_buttons.js
+++ visi-bull/pages/checkboxes_and_radio_buttons/checkboxes_and_radio_buttons.js
@@ -9,6 +9,7 @@ $(document).ready(function()
     create_cross_boxes("#crossbox");
     create_scribble_boxes("#scribblebox");
     create_fancy_radio("#fancyradio");
+    create_fancy_radio_vertical("#fancyradiovert");
 
     /*********************
     * Creation Functions *
@@ -287,19 +288,145 @@ $(document).ready(function()
             });
     }
 
+    function create_fancy_radio_vertical(svgID)
+    {
+        // Definitions needed.
+        var svgWidth = 200;  // The width of the SVG element.
+        var svgHeight = 400;  // The height of the SVG element.
+        var choiceGap = 20;  // The gap between each choice.
+        var startXPos = 10;  // The minimum position on the x axis at which the choices can begin being placed.
+        var widthToFill = 180;  // The width of the space that the choices should fill up.
+        var startYPos = 10;  // The Y position of the top of the first row of choices.
+        var heightToFill = 380;  // The height that the choices should evenly fill up (i.e. one row would be right in the middle, 3 rows at the quartiles).
+        var labelHeight;  // The height of each label (all labels have the same height).
+        var transitionLength = 400;  // The length of time that the checkmarks will take to appear and disappear.
+
+        // Create the SVG element.
+        var svg = d3.select(svgID)
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
+
+        // Determine the width of all the labels.
+        var labelWidths = [];  // The width of each label, with index i in this array corresponding to the label with index i in the labels array.
+        var tempText = svg.selectAll("text")
+            .data(labels)
+            .enter()
+            .append("text")
+                .text(function(d) { return d; });
+        tempText.each(function() { labelWidths.push(d3.select(this).node().getBBox().width); });
+        labelHeight = tempText.node().getBBox().height;
+        tempText.remove();  // Remove the text once the bboxs the labels take up have been computed.
+
+        // Compute the positions of the choices.
+        var choiceData = [];
+        var choiceWidth;  // The width of the current choice.
+        var numberOfRows = 1;  // The number of rows of checkboxes to use.
+        var cumulativeWidth = startXPos + choiceGap;
+        for (var i = 0; i < labels.length; i++)
+        {
+            choiceWidth = labelWidths[i] + choiceGap;
+            if (cumulativeWidth + choiceWidth > widthToFill)
+            {
+                numberOfRows++;
+                cumulativeWidth = startXPos + choiceGap;
+            }
+            choiceData.push({"color": colorCodes[i], "label": labels[i], "labelWidth": labelWidths[i], "rowNumber": numberOfRows, "transX": cumulativeWidth});
+            cumulativeWidth += choiceWidth;
+        }
+        var rowGap = heightToFill / (numberOfRows + 1);  // The gaps to leave between each row.
+        for (var i = 0; i < choiceData.length; i++)
+        {
+            var currentChoice = choiceData[i];
+            currentChoice.transY = startYPos + (rowGap * currentChoice.rowNumber - (labelHeight / 2));
+        }
+
+        // Add the lines showing the boundaries for the choices.
+        svg.append("rect")
+            .classed("outline", true)
+            .attr("x", startXPos)
+            .attr("y", startYPos)
+            .attr("width", widthToFill)
+            .attr("height", heightToFill);
+
+        // Add the choices.
+        var choiceContainers = svg.selectAll(".boxes")
+            .data(choiceData)
+            .enter()
+            .append("g")
+                .classed("checkboxContainer", true)
+                .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+        choiceContainers.append("text")  // The label text.
+            .attr("x", 0)
+            .attr("y", Math.ceil(labelHeight * 4 / 5))
+            .text(function(d) { return d.label; });
+        choiceContainers.on("click", function(d)
+            {
+                if (!d3.select(this).classed("selected"))
+                {
+                    // Only make changes to the selected choice if the clicked on choice was not the selected one.
+
+                    // Remove the old selection (if there is one).
+                    var currentlySelected = svg.select(".selected");
+                    if (!currentlySelected.empty())
+                    {
+                        currentlySelected.classed("selected", false);
+                        currentlySelected.select("text").style("fill", "white");
+                        var checkmark = currentlySelected.select(".checkmark");
+                        var checkmarkLength = checkmark.node().getTotalLength();
+                        checkmark
+                            .style("stroke", "white")
+                            .transition()
+                            .duration(transitionLength)
+                            .ease("linear")
+                            .style("stroke-dashoffset", checkmarkLength)
+                            .remove();
+                    }
+
+                    // Select the clicked on choice.
+                    var container = d3.select(this);
+                    var containedLabel = container.select("text");
+                    container.classed("selected", true);
+                    containedLabel.style("fill", d.color);
+                    var checkmark = container.append("path")
+                        .classed("checkmark", true)
+                        .attr("d", function(d)
+                            {
+                                return "M" + (d.labelWidth + (choiceGap / 6)) + ",0" +
+                                       "C" + (d.labelWidth + (choiceGap / 4)) + "," + (-labelHeight / 5) + "," +
+                                             (-choiceGap / 4) + "," + (-labelHeight / 3) + "," +
+                                             (-choiceGap / 4) + "," + (labelHeight / 2) +
+                                       "C" + (-choiceGap / 4) + "," + (labelHeight * 3 / 2) + "," +
+                                             (d.labelWidth + (choiceGap / 4)) + "," + (labelHeight * 3 / 2) + "," +
+                                             (d.labelWidth + (choiceGap / 2)) + "," + (labelHeight / 3);
+                            })
+                        .style("stroke", d.color);
+
+                    // Transition the checkmark so it doesn't appear all at once.
+                    var checkmarkLength = checkmark.node().getTotalLength();
+                    checkmark
+                        .style("stroke-dasharray", checkmarkLength + " " + checkmarkLength)
+                        .style("stroke-dashoffset", checkmarkLength);
+                    checkmark.transition()
+                        .duration(transitionLength)
+                        .ease("linear")
+                        .style("stroke-dashoffset", 0);
+                }
+            });
+    }
+
     function create_scribble_boxes(svgID)
     {
         // Definitions needed.
-        var svgWidth = 600;  // The width of the SVG element.
-        var svgHeight = 120;  // The height of the SVG element.
+        var svgWidth = 200;  // The width of the SVG element.
+        var svgHeight = 400;  // The height of the SVG element.
         var boxSize;  // The width and height of the checkbox.
         var boxStrokeWidth = 1;  // The stroke width for the checkbox.
         var boxLabelGap = 5;  // The gap between the box and the label.
         var choiceGap = 20;  // The gap between each checkbox/label combos.
-        var startXPos = 20;  // The minimum position on the x axis at which the boxes can begin being placed.
-        var widthToFill = 560;  // The width of the space that the checkboxes should fill up.
+        var startXPos = 10;  // The minimum position on the x axis at which the boxes can begin being placed.
+        var widthToFill = 180;  // The width of the space that the checkboxes should fill up.
         var startYPos = 10;  // The Y position of the top of the first row of boxes.
-        var heightToFill = 100;  // The height that the checkboxes should evenly fill up (i.e. one row would be right in the middle, 3 rows at the quartiles).
+        var heightToFill = 380;  // The height that the checkboxes should evenly fill up (i.e. one row would be right in the middle, 3 rows at the quartiles).
         var checkMark;  // The mark that indicates that a box has been checked.
         var transitionLength = 400;  // The length of time that the checkmarks will take to appear and disappear.
 
