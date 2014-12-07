79b1c8efee71f33e98991f5812a944a71f55beb0
unknown <SimonCB765@gmail.com>
2014-12-07 23:33:33 +0000

    Added some more demos

--- visi-bull/pages/carousels/carouselDemos.js
+++ visi-bull/pages/carousels/carouselDemos.js
@@ -11,6 +11,20 @@ $(document).ready(function()
 
     // Create the multi item infinite scrolling demo.
     multi_infinite("Multi-Inf");
+
+    // Create the centered non-infinite scrolling demo.
+    center_non_infinite("Center-Non-Inf");
+
+    // Create the centered infinite scrolling demo.
+    center_infinite("Center-Inf");
+
+    // Create the variable item width/height demos.
+    var_size("Var-Size-Non-Inf", false, false);
+    var_size("Var-Size-Inf", true, true);
+
+    // Create the variable padding demos.
+    var_padding("Var-Pad-Non-Inf", false, false);
+    var_padding("Var-Pad-Inf", true, true);
 });
 
 // Define the colors used for the demos.
@@ -23,6 +37,133 @@ var numberFill = "white";
 var numberFont = "70px";
 var numberFontWeight = "bold";
 var numberStroke = "none";
+
+function center_infinite(svgID)
+{
+
+    var svg = d3.select("#" + svgID)
+        .attr("width", 450)
+        .attr("height", 150);
+
+    // Setup data used to create the items.
+    var itemWidth = 80;
+    var itemData = [];
+    for (var i = 0; i < 8; i++)
+    {
+        itemData.push(
+            {
+                "height": 100,  // Height of the item.
+                "horizontalPadding": 10,  // Horizontal padding of the item (half on the left and half on the right).
+                "key": i,  // Unique identifier for the item.
+                "rootID": svgID,  // The root of the ID used to refer to the item clip paths.
+                "transX": 0,  // Current X position of the item.
+                "transY": 0,  // Current Y position of the item.
+                "verticalPadding": 10,  // Vertical padding of the item (half on top and half on the bottom).
+                "width": itemWidth  // Width of the item.
+            });
+    }
+
+    // Create the items.
+    var items = svg.selectAll(".item")
+        .data(itemData)
+        .enter()
+        .append("g")
+            .classed("item", true)
+            .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    items.append("rect")
+        .attr("width", function(d) { return d.width; })
+        .attr("height", function(d) { return d.height; })
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    items.append("text")
+        .attr("x", function(d) { return d.width / 2; })
+        .attr("y", function(d) { return d.height / 2; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .style("fill", numberFill)
+        .style("font-size", numberFont)
+        .style("font-weight", numberFontWeight)
+        .style("stroke", numberStroke)
+        .text(function(d) { return d.key; });
+
+    // Create the carousel.
+    var carousel = carouselCreator(items)
+        .width(420)
+        .xLoc(15)
+        .yLoc(5)
+        .isCentered(true)
+        .isDots(true)
+        .itemsToShow(3)
+        .itemsToScrollBy(2)
+        .dotContainerHeight(30)
+        .isInfinite(true)
+        .navArrowWidth(40)
+        .navArrowHeight(40);
+    svg.call(carousel);
+}
+
+function center_non_infinite(svgID)
+{
+
+    var svg = d3.select("#" + svgID)
+        .attr("width", 450)
+        .attr("height", 150);
+
+    // Setup data used to create the items.
+    var itemWidth = 130;
+    var itemData = [];
+    for (var i = 0; i < 8; i++)
+    {
+        itemData.push(
+            {
+                "height": 100,  // Height of the item.
+                "horizontalPadding": 10,  // Horizontal padding of the item (half on the left and half on the right).
+                "key": i,  // Unique identifier for the item.
+                "rootID": svgID,  // The root of the ID used to refer to the item clip paths.
+                "transX": 0,  // Current X position of the item.
+                "transY": 0,  // Current Y position of the item.
+                "verticalPadding": 10,  // Vertical padding of the item (half on top and half on the bottom).
+                "width": itemWidth  // Width of the item.
+            });
+    }
+
+    // Create the items.
+    var items = svg.selectAll(".item")
+        .data(itemData)
+        .enter()
+        .append("g")
+            .classed("item", true)
+            .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    items.append("rect")
+        .attr("width", function(d) { return d.width; })
+        .attr("height", function(d) { return d.height; })
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    items.append("text")
+        .attr("x", function(d) { return d.width / 2; })
+        .attr("y", function(d) { return d.height / 2; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .style("fill", numberFill)
+        .style("font-size", numberFont)
+        .style("font-weight", numberFontWeight)
+        .style("stroke", numberStroke)
+        .text(function(d) { return d.key; });
+
+    // Create the carousel.
+    var carousel = carouselCreator(items)
+        .width(420)
+        .xLoc(15)
+        .yLoc(5)
+        .isCentered(true)
+        .isDots(true)
+        .itemsToShow(2)
+        .itemsToScrollBy(1)
+        .dotContainerHeight(30)
+        .navArrowWidth(40)
+        .navArrowHeight(40);
+    svg.call(carousel);
+}
 
 function multi_infinite(svgID)
 {
@@ -136,7 +277,7 @@ function multi_non_infinite(svgID)
 
     // Create the carousel.
     var carousel = carouselCreator(items)
-        .xLoc(20)
+        .xLoc(5)
         .yLoc(5)
         .isDots(true)
         .itemsToShow(4)
@@ -261,6 +402,152 @@ function single_non_infinite(svgID)
     var carousel = carouselCreator(items)
         .xLoc(20)
         .yLoc(5)
+        .isDots(true)
+        .itemsToShow(1)
+        .itemsToScrollBy(1)
+        .dotContainerHeight(30)
+        .navArrowWidth(40)
+        .navArrowHeight(40);
+    svg.call(carousel);
+}
+
+function var_size(svgID, makeInf, centerIt)
+{
+
+    var svg = d3.select("#" + svgID)
+        .attr("width", 450)
+        .attr("height", 150);
+
+    // Setup data used to create the items.
+    var itemWidth = 0;
+    var itemData = [];
+    for (var i = 0; i < 6; i++)
+    {
+        itemData.push(
+            {
+                "height": 0,  // Height of the item.
+                "horizontalPadding": 10,  // Horizontal padding of the item (half on the left and half on the right).
+                "key": i,  // Unique identifier for the item.
+                "rootID": svgID,  // The root of the ID used to refer to the item clip paths.
+                "transX": 0,  // Current X position of the item.
+                "transY": 0,  // Current Y position of the item.
+                "verticalPadding": 10,  // Vertical padding of the item (half on top and half on the bottom).
+                "width": itemWidth  // Width of the item.
+            });
+    }
+    itemData[0].width = 200
+    itemData[0].height = 50
+    itemData[1].width = 100
+    itemData[1].height = 75
+    itemData[2].width = 75
+    itemData[2].height = 40
+    itemData[3].width = 225
+    itemData[3].height = 100
+    itemData[4].width = 300
+    itemData[4].height = 110
+    itemData[5].width = 180
+    itemData[5].height = 90
+
+    // Create the items.
+    var items = svg.selectAll(".item")
+        .data(itemData)
+        .enter()
+        .append("g")
+            .classed("item", true)
+            .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    items.append("rect")
+        .attr("width", function(d) { return d.width; })
+        .attr("height", function(d) { return d.height; })
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    items.append("text")
+        .attr("x", function(d) { return d.width / 2; })
+        .attr("y", function(d) { return d.height / 2; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .style("fill", numberFill)
+        .style("font-size", 15)
+        .style("font-weight", numberFontWeight)
+        .style("stroke", numberStroke)
+        .text(function(d) { return "W" + d.width + ",H" + d.height; });
+
+    // Create the carousel.
+    var carousel = carouselCreator(items)
+        .width(420)
+        .xLoc(20)
+        .yLoc(0)
+        .isInfinite(makeInf)
+        .isCentered(centerIt)
+        .isDots(true)
+        .itemsToShow(1)
+        .itemsToScrollBy(1)
+        .dotContainerHeight(30)
+        .navArrowWidth(40)
+        .navArrowHeight(40);
+    svg.call(carousel);
+}
+
+function var_padding(svgID, makeInf, centerIt)
+{
+
+    var svg = d3.select("#" + svgID)
+        .attr("width", 450)
+        .attr("height", 150);
+
+    // Setup data used to create the items.
+    var itemWidth = 75;
+    var itemData = [];
+    for (var i = 0; i < 6; i++)
+    {
+        itemData.push(
+            {
+                "height": 50,  // Height of the item.
+                "horizontalPadding": 10,  // Horizontal padding of the item (half on the left and half on the right).
+                "key": i,  // Unique identifier for the item.
+                "rootID": svgID,  // The root of the ID used to refer to the item clip paths.
+                "transX": 0,  // Current X position of the item.
+                "transY": 0,  // Current Y position of the item.
+                "verticalPadding": 10,  // Vertical padding of the item (half on top and half on the bottom).
+                "width": itemWidth  // Width of the item.
+            });
+    }
+    itemData[0].horizontalPadding = 60
+    itemData[1].horizontalPadding = 25
+    itemData[2].horizontalPadding = 40
+    itemData[3].horizontalPadding = 15
+    itemData[4].horizontalPadding = 2
+    itemData[5].horizontalPadding = 30
+
+    // Create the items.
+    var items = svg.selectAll(".item")
+        .data(itemData)
+        .enter()
+        .append("g")
+            .classed("item", true)
+            .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    items.append("rect")
+        .attr("width", function(d) { return d.width; })
+        .attr("height", function(d) { return d.height; })
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    items.append("text")
+        .attr("x", function(d) { return d.width / 2; })
+        .attr("y", function(d) { return d.height / 2; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .style("fill", numberFill)
+        .style("font-size", 15)
+        .style("font-weight", numberFontWeight)
+        .style("stroke", numberStroke)
+        .text(function(d) { return "H Pad-" + d.horizontalPadding; });
+
+    // Create the carousel.
+    var carousel = carouselCreator(items)
+        .width(420)
+        .xLoc(20)
+        .yLoc(5)
+        .isInfinite(makeInf)
+        .isCentered(centerIt)
         .isDots(true)
         .itemsToShow(1)
         .itemsToScrollBy(1)
