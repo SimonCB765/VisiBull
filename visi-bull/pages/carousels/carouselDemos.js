96f6a7ec297aedeca6800ff99d806f6c867b4c58
unknown <SimonCB765@gmail.com>
2014-12-10 15:48:17 +0000

    Added demo for variable size items that are individually draggable

--- visi-bull/pages/carousels/carouselDemos.js
+++ visi-bull/pages/carousels/carouselDemos.js
@@ -29,6 +29,10 @@ $(document).ready(function()
     // Create the basic individually draggable item demos.
     indiv_drag("Indiv-Non-Inf", false, false);
     indiv_drag("Indiv-Inf", true, true);
+
+    // Create the different shape item individually draggable item demos.
+    indiv_variable_item_drag("Indiv-Var-Non-Inf", false, false);
+    indiv_variable_item_drag("Indiv-Var-Inf", true, true);
 });
 
 // Define the colors used for the demos.
@@ -624,6 +628,99 @@ function var_shape(svgID, makeInf, centerIt)
         .yLoc(0)
         .isInfinite(makeInf)
         .isCentered(centerIt)
+        .isDots(true)
+        .itemsToShow(1)
+        .itemsToScrollBy(1)
+        .dotContainerHeight(30)
+        .navArrowWidth(40)
+        .navArrowHeight(40);
+    svg.call(carousel);
+}
+
+function indiv_variable_item_drag(svgID, makeInf, centerIt)
+{
+
+    var svg = d3.select("#" + svgID)
+        .attr("width", 450)
+        .attr("height", 150);
+
+    // Setup data used to create the items.
+    var item = svg.append("g")
+        .datum({"height": 100, "key": 0, "rootID": svgID, "transX": 0, "transY": 0, "width": 100})
+        .classed("item", true)
+        .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    item.append("rect")
+        .attr("width", function(d) { return d.width; })
+        .attr("height", function(d) { return d.height; })
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    item = svg.append("g")
+        .datum({"height": 80, "key": 1, "rootID": svgID, "transX": 0, "transY": 0, "width": 80})
+        .classed("item", true)
+        .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    item.append("circle")
+        .attr("r", 40)
+        .attr("cx", 40)
+        .attr("cy", 40)
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    item = svg.append("g")
+        .datum({"height": 90, "key": 2, "rootID": svgID, "transX": 0, "transY": 0, "width": 90})
+        .classed("item", true)
+        .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    item.append("path")
+        .attr("d", "M0,45L45,90L90,45L45,0Z")
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    item = svg.append("g")
+        .datum({"height": 40, "key": 3, "rootID": svgID, "transX": 0, "transY": 0, "width": 80})
+        .classed("item", true)
+        .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    item.append("ellipse")
+        .attr("rx", 40)
+        .attr("ry", 20)
+        .attr("cx", 40)
+        .attr("cy", 20)
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    item = svg.append("g")
+        .datum({"height": 80, "key": 4, "rootID": svgID, "transX": 0, "transY": 0, "width": 40})
+        .classed("item", true)
+        .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    item.append("ellipse")
+        .attr("rx", 20)
+        .attr("ry", 40)
+        .attr("cx", 20)
+        .attr("cy", 40)
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    item = svg.append("g")
+        .datum({"height": 50, "key": 5, "rootID": svgID, "transX": 0, "transY": 0, "width": 100})
+        .classed("item", true)
+        .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    item.append("rect")
+        .attr("width", function(d) { return d.width; })
+        .attr("height", function(d) { return d.height; })
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+    item = svg.append("g")
+        .datum({"height": 80, "key": 6, "rootID": svgID, "transX": 0, "transY": 0, "width": 80})
+        .classed("item", true)
+        .attr("transform", function(d) { return "translate(" + d.transX + "," + d.transY + ")"; });
+    item.append("path")
+        .attr("d", "M0,0L40,80L80,0Z")
+        .style("fill", fillColor)
+        .style("stroke", strokeType);
+
+    // Create the carousel.
+    var items = svg.selectAll(".item");
+    var carousel = carouselCreator(items)
+        .width(420)
+        .xLoc(20)
+        .yLoc(0)
+        .isInfinite(makeInf)
+        .isCentered(centerIt)
+        .isIndivDrag(true)
         .isDots(true)
         .itemsToShow(1)
         .itemsToScrollBy(1)
