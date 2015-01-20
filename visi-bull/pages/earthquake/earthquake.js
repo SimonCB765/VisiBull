5c65bb066063005f2fd230692cbf30e929e2ae94
unknown <SimonCB765@gmail.com>
2015-01-20 14:52:35 +0000

    Outlined container of the map added

--- visi-bull/pages/earthquake/earthquake.js
+++ visi-bull/pages/earthquake/earthquake.js
@@ -21,18 +21,32 @@ $(document).ready(function()
         .style("left", margin);
 
     // Determine the size and positioning of the histogram slider.
-    var histoLeftPadding = 0.05,  // The histogram slider starts 5% of the width of the SVG element from the left edge of it.
-        histoTopPadding = 0.025,  // The histogram slider starts 2.5% of the height of the SVG element from the top edge of it.
-        histoWidthFrac = 0.8,  // The width of the histogram slider is 80% of the width of the SVG element.
-        histoHeightFrac = 0.15;  // The height of the histogram slider is 15% of the height of the SVG element.
+    var histoLeftPadding = 0.05,  // The fraction of the SVG element's width that should be to the left of the histogram slider's left edge.
+        histoTopPadding = 0.025,  // The fraction of the SVG element's height that should be above the histogram slider's top edge.
+        histoWidthFrac = 0.8,  // The fraction of the SVG element's width that the histogram slider takes up.
+        histoHeightFrac = 0.15;  // The fraction of the SVG element's height that the histogram slider takes up.
     var histoLeftEdge = histoLeftPadding * svgWidth,  // The X coordinate of the left edge of the histogram slider.
         histoTopEdge = histoTopPadding * svgHeight,  // The Y coordinate of the top edge of the histogram slider.
         histoWidth = histoWidthFrac * svgWidth,  // The width of the histogram slider.
         histoHeight = histoHeightFrac * svgHeight;  // The height of the histogram slider.
-    var histogram = histogramSlider(histoWidth, histoHeight)  // Create the histogram slider.
+    var histogram = histogramSliderCreator(histoWidth, histoHeight)  // Create the histogram slider.
         .leftEdge(histoLeftEdge)
         .topEdge(histoTopEdge);
     svg.call(histogram);  // Add the histogram slider to the SVG element.
+
+    // Determine the size and positioning of the map.
+    var mapLeftPadding = histoLeftPadding,  // The fraction of the SVG element's width that should be to the left of the map's left edge.
+        mapTopPadding = histoTopPadding + histoHeightFrac + histoTopPadding + histoTopPadding,  // The fraction of the SVG element's height that should be above the map's top edge.
+        mapWidthFrac = histoWidthFrac,  // The fraction of the SVG element's width that the map takes up.
+        mapHeightFrac = 1 - mapTopPadding - histoTopPadding;  // The fraction of the SVG element's height that the map takes up.
+    var mapLeftEdge = mapLeftPadding * svgWidth,  // The X coordinate of the left edge of the map.
+        mapTopEdge = mapTopPadding * svgHeight,  // The Y coordinate of the top edge of the map.
+        mapWidth = mapWidthFrac * svgWidth,  // The width of the map.
+        mapHeight = mapHeightFrac * svgHeight;  // The height of the map.
+    var map = mapCreator(mapWidth, mapHeight)  // Create the map.
+        .leftEdge(mapLeftEdge)
+        .topEdge(mapTopEdge);
+    svg.call(map);  // Add the histogram slider to the SVG element.
 
     // Attach the listener for resize events, and fire it once to size the SVG.
     var windowSelection = $(window);
@@ -52,11 +66,17 @@ $(document).ready(function()
             histoTopEdge = histoTopPadding * svgHeight;
             histoWidth = histoWidthFrac * svgWidth;
             histoHeight = histoHeightFrac * svgHeight;
+            
+            // Update the map.
+            mapLeftEdge = mapLeftPadding * svgWidth;
+            mapTopEdge = mapTopPadding * svgHeight;
+            mapWidth = mapWidthFrac * svgWidth;
+            mapHeight = mapHeightFrac * svgHeight;
         });
     $(window).trigger( "resize" );
 });
 
-function histogramSlider(width, height)
+function histogramSliderCreator(width, height)
 {
     /***************************
     * Default Parameter Values *
@@ -118,4 +138,68 @@ function histogramSlider(width, height)
     }
 
     return histoSlider;
+}
+
+function mapCreator(width, height)
+{
+    /***************************
+    * Default Parameter Values *
+    ***************************/
+    var leftEdge = 0,  // The X coordinate of the left edge of the map.
+        topEdge = 0,  // The Y coordinate of the top edge of the map.
+        width = width,  // The width of the map.
+        height = height;  // The height of the map.
+
+    /************************
+    * Map Creation Function *
+    ************************/
+    function map(selection)
+    {
+        // Setup the map container.
+        var mapContainer = selection.append("g")
+            .attr("transform", function() { return "translate(" + leftEdge + "," + topEdge + ")"; });
+
+        // Create the backing rectangle.
+        var backingRect = mapContainer.append("rect")
+            .classed("mapBackingRect", true)
+            .attr("width", width)
+            .attr("height", height);
+    }
+
+    /**********************
+    * Getters and Setters *
+    **********************/
+    // Get/Set the X coordinate of the left edge of the map.
+    map.leftEdge = function(_)
+    {
+        if (!arguments.length) return leftEdge;
+        leftEdge = _;
+        return map;
+    }
+
+    // Get/Set the Y coordinate of the top edge of the map.
+    map.topEdge = function(_)
+    {
+        if (!arguments.length) return topEdge;
+        topEdge = _;
+        return map;
+    }
+
+    // Get/Set the width of the map.
+    map.width = function(_)
+    {
+        if (!arguments.length) return width;
+        width = _;
+        return map;
+    }
+
+    // Get/Set the height of the map.
+    map.height = function(_)
+    {
+        if (!arguments.length) return height;
+        height = _;
+        return map;
+    }
+
+    return map;
 }
